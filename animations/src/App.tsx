47d415242301941203e4cbd0773b64b0e94fe2dc
zelonKim <ksz18601@gmail.com>
2023-06-30 12:56:27 +0900

    useMotionValue & useTransform

--- animations/src/App.tsx
+++ animations/src/App.tsx
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import styled from "styled-components";
-import { motion } from "framer-motion";
+import {
+  motion,
+  useMotionValue,
+  useMotionValueEvent,
+  useTransform,
+  useScroll,
+} from "framer-motion";
 
-const Wrapper = styled.div`
-  height: 100vh;
+const Wrapper = styled(motion.div)`
+  height: 200vh;
   width: 100vw;
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: #e423ae;
+  background: linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238));
+`;
+
+const BiggerBox = styled.div`
+  width: 600px;
+  height: 600px;
+  background-color: rgba(255, 255, 255, 0.4);
+  border-radius: 40px;
+  dispaly: flex;
+  justify-content: center;
+  align-item: center;
+  overflow: hidden;
 `;
 
 const Box = styled(motion.div)`
@@ -81,16 +98,74 @@ function App() {
 }
 export default App; */
 
+/* 
 const boxVari = {
   hover: { scale: 2, rotateZ: 90 },
   click: { scale: 1, borderRadius: "100px" },
+  draging: { backgroundColor: "rgb(0, 0, 0)", transition: { duration: 10 } },
 };
 
-function App() {
+const biggerBoxRef = useRef<HTMLDivElement>(null);
   return (
     <Wrapper>
-      <Box variants={boxVari} whileHover="hover" whileTap="click" />
+      <BiggerBox ref={biggerBoxRef}>
+        <Box
+          drag
+          dragSnapToOrigin
+          dragElastic={1}
+          dragConstraints={biggerBoxRef}
+          variants={boxVari}
+          whileHover="hover"
+          whileTap="click"
+          whileDrag="draging"
+        />
+      </BiggerBox>
+    </Wrapper>
+  );
+  }
+export default App;
+*/
+// whileDrag 프로퍼티의 값으로 문자 색상("black")이 아닌, 숫자 색상"rgb(0, 0, 0)"을 써야 애니메이트 됨.
+
+/* 
+function App() {
+  const motionValue = useMotionValue(0);
+  const scaling = useTransform(motionValue, [-800, 0, 800], [2, 1, 0.1]);
+  const rotating = useTransform(motionValue, [-800, 800], [-360, 360]);
+  const gradient = useTransform(
+    motionValue,
+    [-800, 0, 800],
+    [
+      "linear-gradient(135deg, rgb(0, 210, 238), rgb(0, 83, 238))",
+      "linear-gradient(135deg, rgb(238,0,153), rgb(221, 0, 238))",
+      "linear-gradient(135deg, rgb(0,238,155), rgb(238, 178, 0))",
+    ]
+  );
+
+
+  const { scrollY, scrollYProgress } = useScroll();
+
+
+ useMotionValueEvent(scrollY, "change", (a) => {
+    console.log("scrollY:", a);
+  });
+
+  useMotionValueEvent(scrollYProgress, "change", (a) => {
+    console.log("scrollYProgress:", a);
+  }); 
+
+
+  const scrolling = useTransform(scrollYProgress, [0,1], [1,5])
+
+  return (
+    <Wrapper style={{ background: gradient }}>
+      <Box
+        style={{ x: motionValue, scale: scrolling, rotateZ: rotating }}
+        drag="x"
+        dragSnapToOrigin
+      />
     </Wrapper>
   );
 }
 export default App;
+*/
