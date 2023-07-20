eba26194b4976ee260e7075b0809af1218277df2
zelonKim <ksz18601@gmail.com>
2023-07-20 14:06:19 +0900

    catch all Route

--- nextjs/pages/index.js
+++ nextjs/pages/index.js
@@ -42,6 +42,7 @@ import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 
 
+/* 
 export default function Home({ results }) {
   const router = useRouter()
   
@@ -57,54 +58,55 @@ export default function Home({ results }) {
   }
 
   return (
-      <div className="container">
-          <Seo title="Home" />
-          
-          {results?.map((movie) => (
-              <div onClick={() => onClick(movie.id, movie.original_title)} className="movie" key={movie.id}>
-                <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
-                <Link 
-                  href={{
+    <div className="container">
+        <Seo title="Home" />
+        
+        {results?.map((movie) => (
+            <div onClick={() => onClick(movie.id, movie.original_title)} className="movie" key={movie.id}>
+              <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
+              
+           <Link 
+                 href={{
                     pathname: `/movies/${movie.id}`,
                     query: {
                       title: movie.original_title
                     }
                     }}
                     as={`/movies/${movie.id}`}>
-                  <h4> {movie.original_title} </h4>
+                  <h4> {movie.original_title} </h4> 
+                </ Link>
+*/
+
+
+
+export default function Home({ results }) {
+    const router = useRouter()
+    
+    const onClick = (id, title) => { 
+      router.push(`/movies/${id}/${title}`)
+    }
+
+  return (
+      <div className="container">
+          <Seo title="Home" />
+          
+          {results.map((movie) => (
+              <div onClick={() => onClick(movie.id, movie.original_title)} className="movie" key={movie.id}>
+                <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
+                <Link 
+                   href={`/movies/${movie.id}/${movie.original_title}`}>
+                    <h4> {movie.original_title} </h4> 
                 </Link>
               </div>
           ))}
-          
-        <style jsx>{`
-          .container {
-            display: grid;
-            grid-template-columns: 1fr 1fr;
-            padding: 20px;
-            gap: 20px;
-          }
-          .movie img {
-            max-width: 100%;
-            border-radius: 12px;
-            transition: transform 0.2s ease-in-out;
-            box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
-          }
-          .movie:hover img {
-            transform: scale(1.05) translateY(-10px);
-          }
-          .movie h4 {
-            font-size: 18px;
-            text-align: center;
-          }
-      `}</style>
-      
+        
       </div>
     )
 } 
 
 
 export async function getServerSideProps() {
-  const { results } = await (await fetch(`http://localhost:3000/api/movies`)).json();
+  const { results } = await(await fetch(`http://localhost:3000/api/movies`)).json();
   
   return ({
     props: {
